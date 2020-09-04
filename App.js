7866068f3dde1cf9b2df9f7daf49522c30fa8594
Vikash <vikashr370@gmail.com>
2020-09-04 21:54:22 +0530

    init

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import TabScreen from './src/screens/TabScreen'
+
+export default class App extends Component {
+  render(){
+    return (
+    <TabScreen/>
+      
+      
+    );
+  }
+  
+};
+
+const styles = StyleSheet.create({
+ 
+ 
+
+});
+
+
---
+++ src/screens/TabScreen.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Tab, Tabs,Left,Body,Title,Right } from 'native-base';
+import Tab1 from './tabs/tab1';
+import Tab2 from './tabs/tab2';
+import Tab3 from './tabs/tab3';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header style={{backgroundColor:'#009387'}} hasTabs>
+        <Left/>
+          <Body>
+            <Title style={{color:"white"}}>News App </Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs tabBarUnderlineStyle={{backgroundColor:'white'}}>
+          <Tab tabStyle={{backgroundColor:'#009387'}}
+          activeTabStyle={{backgroundColor:'#009387'}}
+          textStyle={{color:'white'}}
+          activeTextStyle={{color:'white'}}
+          heading="Tab1">
+            <Tab1 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#009387'}} 
+           activeTabStyle={{backgroundColor:'#009387'}}
+           textStyle={{color:'white'}}
+           activeTextStyle={{color:'white'}}
+          heading="Tab2">
+            <Tab2 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#009387'}} 
+           activeTabStyle={{backgroundColor:'#009387'}}
+           textStyle={{color:'white'}}
+           activeTextStyle={{color:'white'}}
+          heading="Tab3">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
